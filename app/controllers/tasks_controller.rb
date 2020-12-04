a6c93b3942ed54fa56640d5d2ad0fa3dc9c397c7
seiha.kuwatori <aonami98@gmail.com>
2020-12-04 12:03:12 +0000

    kadi-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,8 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   
   def index
-    if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
